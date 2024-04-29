91b009dd350b3acf828e86f84af206fb9a6e90ca
NguyenHongThinh-2003 <ocbu0212@gmail.com>
2024-04-29 22:18:43 +0700

    add meetingCart and showing modal when click

--- components/ui/MeetingModal.tsx
+++ components/ui/MeetingModal.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { ReactNode } from "react";
 import {
   Dialog,
@@ -7,7 +8,9 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+import { Button } from "./button";
 interface MeetingModalProps {
   isOpen: boolean;
   onClose: () => void;
@@ -35,21 +38,40 @@ const MeetingModal = ({
   buttonClassName,
   buttonIcon,
 }: MeetingModalProps) => {
+  console.log(isOpen);
   return (
-    <div>
-      <Dialog>
-        <DialogTrigger>Open</DialogTrigger>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Are you absolutely sure?</DialogTitle>
-            <DialogDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
-            </DialogDescription>
-          </DialogHeader>
-        </DialogContent>
-      </Dialog>
-    </div>
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogContent className="flex w-full max-w-[520px] flex-col gap-6 border-none bg-dark-1 px-6 py-9 text-white">
+        <div className="flex flex-col gap-6">
+          {image && (
+            <div className="flex justify-center">
+              <Image src={image} alt="checked" width={72} height={72} />
+            </div>
+          )}
+          <h1 className={cn("text-3xl font-bold leading-[42px]", className)}>
+            {title}
+          </h1>
+          {children}
+          <Button
+            className={
+              "bg-blue-1 focus-visible:ring-0 focus-visible:ring-offset-0"
+            }
+            onClick={handleClick}
+          >
+            {buttonIcon && (
+              <Image
+                src={buttonIcon}
+                alt="button icon"
+                width={13}
+                height={13}
+              />
+            )}{" "}
+            &nbsp;
+            {buttonText || "Schedule Meeting"}
+          </Button>
+        </div>
+      </DialogContent>
+    </Dialog>
   );
 };
 
--- components/ui/MeetingTypeList.tsx
+++ components/ui/MeetingTypeList.tsx
@@ -2,47 +2,98 @@ import Image from "next/image";
 import React, { useState } from "react";
 import HomeCart from "./HomeCart";
 import MeetingModal from "./MeetingModal";
+import { useUser } from "@clerk/nextjs";
+import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
+import { useRouter } from "next/navigation";
 
-const MeetingTypeList = () => {
-  const [meetingState, setmeetingState] = useState<
-    "isScheduleMeeting " | "isJoiningMeeting" | "isInstantMeeting" | undefined
-  >();
+function MeetingTypeList() {
+  const [meetingState, setMeetingState] = useState<
+    "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
+  >(undefined);
+  const [callDetail, setCallDetail] = useState<Call>();
+  const client = useStreamVideoClient();
+  const { user } = useUser();
+  const [value, setvalue] = useState({
+    dateTime: new Date(),
+    description: "",
+    link: "",
+  });
+  const initialValues = {
+    dateTime: new Date(),
+    description: "",
+    link: "",
+  };
+  const [values, setValues] = useState(initialValues);
 
-  const createMeeting = () => {};
+  const router = useRouter();
+  const createMeeting = async () => {
+    if (!client || !user) return;
+    try {
+      if (!values.dateTime) {
+        // toast({ title: 'Please select a date and time' });
+        return;
+      }
+      const id = crypto.randomUUID();
+      const call = client.call("default", id);
+      if (!call) throw new Error("Failed to create meeting");
+      const startsAt =
+        values.dateTime.toISOString() || new Date(Date.now()).toISOString();
+      const description = values.description || "Instant Meeting";
+      await call.getOrCreate({
+        data: {
+          starts_at: startsAt,
+          custom: {
+            description,
+          },
+        },
+      });
+      setCallDetail(call);
+      if (!values.description) {
+        router.push(`/meeting/${call.id}`);
+      }
+      // toast({
+      //   title: 'Meeting Created',
+      // });
+    } catch (error) {
+      console.error(error);
+      // toast({ title: 'Failed to create Meeting' });
+    }
+  };
+
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
       <HomeCart
         img="/icons/add-meeting.svg"
         title="New meeting"
         description="Start an instant meeting"
-        handleClick={() => setmeetingState("isJoiningMeeting")}
+        handleClick={() => setMeetingState("isInstantMeeting")}
         classname=" bg-orange-1 "
       />
       <HomeCart
         img="/icons/join-meeting.svg"
         title="Schdule Meeting"
         description="Plan your meeting"
-        handleClick={() => setmeetingState("isJoiningMeeting")}
+        handleClick={() => setMeetingState("isScheduleMeeting")}
         classname=" bg-blue-1 "
       />
       <HomeCart
         img="/icons/add-meeting.svg"
         title="New meeting"
         description="Start an instant meeting"
-        handleClick={() => setmeetingState("isJoiningMeeting")}
+        handleClick={() => setMeetingState("isJoiningMeeting")}
         classname=" bg-purple-1 "
       />
       <HomeCart
         img="/icons/join-meeting.svg"
         title="Join Meeting"
         description="via invitation link"
-        handleClick={() => setmeetingState("isJoiningMeeting")}
+        handleClick={() => setMeetingState("isJoiningMeeting")}
         classname="bg-yellow-1 "
       />
 
       <MeetingModal
         isOpen={meetingState === "isInstantMeeting"}
-        onClose={() => setmeetingState(undefined)}
+        onClose={() => setMeetingState(undefined)}
         title="Start an instant meeting"
         className="text-center"
         buttonText="Start meeting"
@@ -50,6 +101,6 @@ const MeetingTypeList = () => {
       />
     </section>
   );
-};
+}
 
 export default MeetingTypeList;
