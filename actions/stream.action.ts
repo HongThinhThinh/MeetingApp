449c41b565a5c84b2723d82028e113695b3c7eba
NguyenHongThinh-2003 <ocbu0212@gmail.com>
2024-04-30 17:13:19 +0700

    add public key and api key

--- actions/stream.action.ts
+++ actions/stream.action.ts
@@ -3,8 +3,9 @@
 import { currentUser } from "@clerk/nextjs/server";
 import { StreamClient } from "@stream-io/node-sdk";
 
-const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
-const apiSecret = process.env.STREAM_SECRET_KEY;
+const apiKey = "fd62za675hmg";
+const apiSecret =
+  "bz626pyu6jh5d7hmehhvxtznywsvyamy6y6r89wqktft2ypfaca62s4zahhpj9nh";
 
 export const tokenProvider = async () => {
   const user = await currentUser();
@@ -13,10 +14,9 @@ export const tokenProvider = async () => {
   if (!apiKey) throw new Error("No API key");
   if (!apiSecret) throw new Error("No API secret");
 
-
-  const client = new StreamClient(apiKey, apiSecret)
+  const client = new StreamClient(apiKey, apiSecret);
   const exp = Math.round(new Date().getTime() / 1000) + 60 * 60;
-  const issued = Math.floor(Date.now()/1000)-60
-  const token = client.createToken(user.id,exp, issued)
+  const issued = Math.floor(Date.now() / 1000) - 60;
+  const token = client.createToken(user.id, exp, issued);
   return token;
 };
