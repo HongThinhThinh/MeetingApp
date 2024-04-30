6cc0d1d3abe394f0b9b3b9644ce4bad8b9c8747f
NguyenHongThinh-2003 <ocbu0212@gmail.com>
2024-04-30 17:18:05 +0700

    config secrectkey

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    CLERK_SECRET_KEY:
+      "bz626pyu6jh5d7hmehhvxtznywsvyamy6y6r89wqktft2ypfaca62s4zahhpj9nh",
+  },
+};
 
 export default nextConfig;
